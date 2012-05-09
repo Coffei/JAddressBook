bdd8fcc6aa5dbe6d371bea3a8cfe4fa520035896
Coffei <Trantina.Jonas@gmail.com>
2012-05-09 14:53:26 +0200

    Semifinished GroupForm, changes to pom

--- src/main/java/cz/muni/fi/coffei/addressbook/gui/GroupForm.java
+++ src/main/java/cz/muni/fi/coffei/addressbook/gui/GroupForm.java
@@ -4,15 +4,21 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Collections;
+import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.InputVerifier;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -21,19 +27,30 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.ListCellRenderer;
+import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.text.JTextComponent;
 
+import cz.muni.fi.pv168.DBUtils;
 import cz.muni.fi.pv168.Group;
+import cz.muni.fi.pv168.GroupManager;
 import cz.muni.fi.pv168.Person;
+import cz.muni.fi.pv168.PersonManager;
+import cz.muni.fi.pv168.ServiceFailureException;
 
 public class GroupForm extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private DefaultListModel<BoolWrapper<Person>> model;
+
+	private final Group group;
+	private List<Person> assignedPeople = Collections.emptyList();
+
 
 	/**
 	 * Launch the application.
@@ -47,11 +64,11 @@ public class GroupForm extends JDialog {
 				} catch (ClassNotFoundException | InstantiationException
 						| IllegalAccessException
 						| UnsupportedLookAndFeelException e) {
-						e.printStackTrace();
+					e.printStackTrace();
 				}
 			}
 		}
-		
+
 		try {
 			GroupForm dialog = new GroupForm(null);
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
@@ -66,23 +83,10 @@ public class GroupForm extends JDialog {
 	 * @param group group to be modified or null if creating new
 	 */
 	public GroupForm(Group group) {
-		//data, tepmorary
-		Person p1 = new Person();
-		Person p2 = new Person();
-		Person p3 = new Person();
-		Person p4 = new Person();
-		Person p5 = new Person();
-		p1.setName("Marko");
-		p2.setName("Adam");
-		p3.setName("Erik");
-		p4.setName("Pavel");
-		p5.setName("Tereza");
-		///data
-		
-		
-		
+		this.group = group;
+
 		setTitle("Group form");
-		
+
 		setBounds(100, 100, 500, 340);
 		setMinimumSize(new Dimension(200, 200));
 		getContentPane().setLayout(new BorderLayout());
@@ -94,42 +98,36 @@ public class GroupForm extends JDialog {
 		panel_1.setBorder(new TitledBorder(null, "People", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
 		gl_contentPanel.setHorizontalGroup(
-			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				gl_contentPanel.createParallelGroup(Alignment.LEADING)
 				.addComponent(panel, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
 				.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-		);
+				);
 		gl_contentPanel.setVerticalGroup(
-			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				gl_contentPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 213, Short.MAX_VALUE))
-		);
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 213, Short.MAX_VALUE))
+				);
 		panel_1.setLayout(new BorderLayout(0, 0));
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBorder(null);
 		panel_1.add(scrollPane, BorderLayout.CENTER);
-		
+
 		JList<BoolWrapper<Person>> list = new JList<BoolWrapper<Person>>();
 		list.setBackground(UIManager.getColor("List.background"));
 		list.setLayoutOrientation(JList.VERTICAL_WRAP);
 		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		list.setCellRenderer(CHECKBOX_LIST_RENDERER);
-		DefaultListModel<BoolWrapper<Person>> listModel = new DefaultListModel<>();
-		listModel.addElement(new BoolWrapper<Person>(p1));
-		listModel.addElement(new BoolWrapper<Person>(p2, true));
-		listModel.addElement(new BoolWrapper<Person>(p3, true));
-		listModel.addElement(new BoolWrapper<Person>(p4));
-		listModel.addElement(new BoolWrapper<Person>(p5));
-		list.setModel(listModel);
+		list.setModel(loadPeople());
 		list.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				listClicked(e);
 			}
 		});
-		
+
 		scrollPane.setViewportView(list);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		{
@@ -141,11 +139,17 @@ public class GroupForm extends JDialog {
 				JLabel lblName = new JLabel("Name");
 				panel_2.add(lblName, BorderLayout.WEST);
 			}
-			
-			
-			JTextPane textPane = new JTextPane();
-			textPane.setPreferredSize(new Dimension(12, 12));
-			panel_2.add(textPane);
+
+
+			JTextPane nameText = new JTextPane();
+			nameText.setPreferredSize(new Dimension(12, 12));
+			nameText.setInputVerifier(new InputVerifier() {
+				public boolean verify(JComponent input) { //put all verification-dependent code here
+					String text = ((JTextComponent)input).getText();
+					return text!=null && !text.isEmpty() && text.length() < 255;
+				}
+			});
+			panel_2.add(nameText);
 		}
 		contentPanel.setLayout(gl_contentPanel);
 		{
@@ -154,26 +158,97 @@ public class GroupForm extends JDialog {
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						okClicked(e);
+					}
+				});
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						cancelClicked(e);
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
 	}
-	
+
+	/**
+	 * Loads people from data source and creates according ListModel.
+	 * @param group group to edit, null if creating new
+	 * @return ListModel ready to assign to JList
+	 */
+	private ListModel<BoolWrapper<Person>> loadPeople() {
+		GroupManager groupman = DBUtils.getAppContext().getBean("groupManager", GroupManager.class);
+		PersonManager personman = DBUtils.getAppContext().getBean("personManager", PersonManager.class);
+		model = new DefaultListModel<>();
+		if(group!=null) {
+			try {
+				for(Person p : (assignedPeople = groupman.findAllPersonsInGroup(group))) {
+					model.addElement(new BoolWrapper<Person>(p, true));
+				}
+			} catch (ServiceFailureException e) {
+				//TODO: exception handling
+			}
+		}
+
+		List<Person> allPeople = personman.findAllPersons();
+		allPeople.removeAll(assignedPeople);
+		for(Person p : allPeople ) {
+			model.addElement(new BoolWrapper<Person>(p));
+		}
+
+		return model;
+	}
+
+	protected void cancelClicked(ActionEvent e) {
+		this.setVisible(false);
+		this.dispose();
+
+	}
+
+	protected void okClicked(ActionEvent e) {// Find changes and save them
+		GroupManager groupman = DBUtils.getAppContext().getBean("groupManager", GroupManager.class);
+
+		//Name
+		try {
+
+			for(Person p : assignedPeople) {
+				if(!model.contains(new BoolWrapper<Person>(p))) {
+					groupman.removePersonFromGroup(p, group);
+				}
+			}
+
+			for (int i = 0; i < model.getSize(); i++) {
+				BoolWrapper<Person> w = model.get(i);
+				if(w.value && !assignedPeople.contains(w.object)) {
+					groupman.addPersonToGroup(w.object, group);
+				}
+			}
+
+		} catch (ServiceFailureException sfex) {
+			//TODO:Exception handling
+		}
+
+
+
+	}
+
 	//JList click event, change bool value of particular BoolWraper and repaint to show change
 	protected void listClicked(MouseEvent e) {
-		JList source = (JList)e.getSource();
+		JList<BoolWrapper<Person>> source = (JList<BoolWrapper<Person>>)e.getSource();
 		int index = source.getSelectedIndex();
 		if(index>=0) {
-				BoolWrapper<Person> wrapper = ((DefaultListModel<BoolWrapper<Person>>)source.getModel()).get(index);
-				wrapper.value ^= true; //negate wrapper value
-				source.repaint();
+			BoolWrapper<Person> wrapper = ((DefaultListModel<BoolWrapper<Person>>)source.getModel()).get(index);
+			wrapper.value ^= true; //negate wrapper value
+			source.repaint();
 		}
 	}
 
@@ -185,20 +260,20 @@ public class GroupForm extends JDialog {
 		@Override
 		public Component getListCellRendererComponent(
 				JList<? extends BoolWrapper<Person>> list,
-				BoolWrapper<Person> value, int index, boolean isSelected,
-				boolean cellHasFocus) {
+						BoolWrapper<Person> value, int index, boolean isSelected,
+						boolean cellHasFocus) {
 			JCheckBox check = new JCheckBox();
 			if(value !=null) {
 				check.setText(value.object.getName());
 				check.setSelected(value.getValue());
 			}
-			
+
 			return check;
 		}
 	};
-	
-	
-	
+
+
+
 	/**
 	 * Class to manage a bond between E object and boolean
 	 * @author Coffei
@@ -208,7 +283,7 @@ public class GroupForm extends JDialog {
 	private class BoolWrapper<E>{
 		private E object;
 		private boolean value;
-		
+
 		/**
 		 * Constructor
 		 * @param object object to be bonded, can not be null
@@ -220,7 +295,7 @@ public class GroupForm extends JDialog {
 			this.object = object;
 			this.value = value;
 		}
-		
+
 		/**
 		 * Constructor
 		 * @param object object to be bonded with false
@@ -228,7 +303,7 @@ public class GroupForm extends JDialog {
 		public BoolWrapper(E object) {
 			this(object, false);
 		}
-		
+
 		public E getObject() {
 			return object;
 		}
@@ -238,8 +313,32 @@ public class GroupForm extends JDialog {
 		public void setValue(boolean value) {
 			this.value = value;
 		}
-		
-		
-	
+
+		@Override
+		public int hashCode() {
+			return object.hashCode() * 31;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			BoolWrapper<?> other = (BoolWrapper<?>) obj;
+			if (object == null) {
+				if (other.object != null)
+					return false;
+			} else if (!object.equals(other.object))
+				return false;
+			return true;
+		}
+
+
+
+
+
 	}
 }
